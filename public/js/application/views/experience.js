a78eb3e348dcfedc6d60a1373202916a83fb2403
Aaron Lote <aaron.lote@gmail.com>
2015-04-03 12:05:29 +0100

    Finished implementing aayying ya'll

--- public/js/application/views/experience.js
+++ public/js/application/views/experience.js
@@ -1,18 +1,16 @@
 define(
   [
     'text!/js/application/templates/experience.html',
-    'application/collections/experience',
     'application/views/base'
   ],
-  function (Template, Collection, Base) {
+  function (Template, Base) {
 
-    return Base.extend({
-      collection: Collection,
-      options: _.extend({}, Base.prototype.options, {
-        templates: {
-          main: Template
-        }
-      })
+  return Base.extend({
+    options: _.extend({}, Base.prototype.options, {
+      templates: {
+        main: Template
+      }
     })
-  }
+  })
+}
 )
